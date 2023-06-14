8a5bd34d3ee63c61294bf9381e6ec290460901a0
garcia-f <garciafabianemmanuel@gmail.com>
2023-06-13 22:17:11 -0300

    ultima modificacion

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 require('dotenv').config();
-const puerto = process.env.puerto;
+const port = process.env.port;
 const app = express();
 
 
@@ -16,7 +16,7 @@ app.use(express.json());
 
 app.use('/',require('./routes/app.routes'));
 
-app.listen(puerto || 5000,()=>{
-    console.log(`servidor escuchando en http://localhost:${puerto}`);
+app.listen(port || 6000, ()=>{
+    console.log(`servidor escuchando en http://localhost:${port}`);
 })
 
--- routes/app.routes.js
+++ routes/app.routes.js
@@ -3,10 +3,10 @@ const routes=express.Router();
 const {mensajeGet, mensajePost, mensajePut, mensajeDelete}=require('../controllers/controllers.js');
 
 
-routes.get('/', mensajeGet);
-routes.put('/', mensajePut);
-routes.post('/', mensajePost);
-routes.delete('/', mensajeDelete);
+routes.get('/get', mensajeGet);
+routes.put('/put', mensajePut);
+routes.post('/post', mensajePost);
+routes.delete('/delete', mensajeDelete);
 
 
 module.exports = routes;
